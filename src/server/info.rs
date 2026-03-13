170eaf00ca83d35e937d0a9a3635f8f6a933fab1
David Moravec <moravakovec@gmail.com>
2026-03-13 11:44:32 +0100

    feat: add repl_id (hardcoded) and repl_offset (always 0)

--- src/server/info.rs
+++ src/server/info.rs
@@ -3,14 +3,14 @@ use bytes::Bytes;
 use std::fmt::Display;
 
 enum Role {
-    Master,
+    Master { repl_id: String, repl_offset: usize },
     Slave(String),
 }
 
 impl Display for Role {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Master => write!(f, "master"),
+            Self::Master { .. } => write!(f, "master"),
             Self::Slave(_) => write!(f, "slave"),
         }
     }
@@ -24,7 +24,10 @@ impl Replication {
     fn new(replica_of: Option<String>) -> Self {
         let role = match replica_of {
             Some(s) => Role::Slave(s),
-            None => Role::Master,
+            None => Role::Master {
+                repl_id: "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb".to_string(),
+                repl_offset: 0,
+            },
         };
 
         Self { role }
@@ -33,7 +36,16 @@ impl Replication {
 
 impl ToFrame for Replication {
     fn to_frame(&self) -> Frame {
-        Frame::BulkString(Bytes::from(format!("role:{}", self.role)))
+        match self.role {
+            Role::Slave(_) => Frame::BulkString(Bytes::from(format!("role:{}", self.role))),
+            Role::Master {
+                ref repl_id,
+                ref repl_offset,
+            } => Frame::BulkString(Bytes::from(format!(
+                "role:{}\nmaster_replid:{}\nmaster_repl_offset:{:}",
+                self.role, repl_id, repl_offset
+            ))),
+        }
     }
 }
 
