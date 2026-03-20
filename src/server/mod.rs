564f464c9ca24205d812c77c8d5e69d9b82bc821
David Moravec <moravakovec@gmail.com>
2026-03-20 08:34:36 +0100

    test: add getack test

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -225,3 +225,163 @@ impl Handle {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::time::Duration;
+
+    use tokio::io::AsyncWriteExt;
+
+    use super::*;
+
+    async fn start_master(port: &str) -> SocketAddr {
+        let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{:}", port))
+            .await
+            .unwrap();
+
+        let server = Server::new(None);
+        let local_addres = listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            if let Err(e) = server.run(listener).await {
+                eprintln!(
+                    "when running server on port {:} following error occured; {}",
+                    local_addres, e
+                );
+            }
+        });
+
+        local_addres
+    }
+
+    async fn start_replica(master_addr: String, port: &str) -> SocketAddr {
+        let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{:}", port))
+            .await
+            .unwrap();
+
+        let server = Server::new(Some(master_addr));
+        let local_addres = listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            if let Err(e) = server.run(listener).await {
+                eprintln!(
+                    "when running server on port {:} following error occured; {}",
+                    local_addres, e
+                );
+            }
+        });
+
+        local_addres
+    }
+
+    async fn replication_handshake(master_replica_connection: &mut Connection) -> Result<()> {
+        master_replica_connection.read_frame().await?;
+        master_replica_connection.send_command(&["PONG"]).await?;
+        // REPLCONF 1,
+        master_replica_connection.read_frame().await?;
+        master_replica_connection
+            .write_frame(&Frame::Simple("OK".to_string()))
+            .await?;
+        // REPLCONF2,
+        master_replica_connection.read_frame().await?;
+        master_replica_connection
+            .write_frame(&Frame::Simple("OK".to_string()))
+            .await?;
+        master_replica_connection.read_frame().await?;
+        master_replica_connection
+            .write_frame(&Frame::Simple(
+                "FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0".to_string(),
+            ))
+            .await?;
+        let rdb_file = Bytes::from(hex::decode(
+            "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2",
+        ).unwrap());
+        master_replica_connection.write_rdb_file(rdb_file).await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_replica() {
+        // let master_address = start_master(None, "0").await;
+        let master_listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let master_address = master_listener.local_addr().unwrap();
+
+        let replica_address = start_replica(
+            format!("{} {}", master_address.ip(), master_address.port()),
+            "0",
+        )
+        .await;
+
+        let replication_socket = master_listener.accept().await.unwrap();
+
+        let master_replica_stream = replication_socket.0;
+
+        let tx: Sender<Frame> = broadcast::channel(16).0;
+
+        let mut master_replica_connection = Connection::new(
+            master_replica_stream,
+            Arc::new(ServerInfo::new(None)),
+            Arc::new(tx),
+        );
+
+        replication_handshake(&mut master_replica_connection)
+            .await
+            .unwrap();
+
+        master_replica_connection
+            .send_command(&["REPLCONF", "GETACK", "*"])
+            .await
+            .unwrap();
+
+        match tokio::time::timeout(
+            Duration::from_millis(500),
+            master_replica_connection.read_frame(),
+        )
+        .await
+        {
+            Ok(frame_result) => {
+                eprintln!("{:?}", frame_result);
+                assert!(
+                    frame_result.unwrap().unwrap().to_bytes().to_vec()
+                        == b"*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"
+                );
+            }
+            Err(_) => {
+                eprintln!("timeout reached");
+                assert!(false)
+            }
+        }
+        master_replica_connection
+            .send_command(&["PING"])
+            .await
+            .unwrap();
+        // eprintln!("Ping response: {}")master_replica_connection
+        //     .read_frame()
+        //     .await
+        //     .unwrap()
+        //     .unwrap();
+        master_replica_connection
+            .send_command(&["REPLCONF", "GETACK", "*"])
+            .await
+            .unwrap();
+
+        match tokio::time::timeout(
+            Duration::from_millis(500),
+            master_replica_connection.read_frame(),
+        )
+        .await
+        {
+            Ok(frame_result) => {
+                eprint!("{:?}", frame_result);
+                assert!(
+                    frame_result.unwrap().unwrap().to_bytes().to_vec()
+                        == b"*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$2\r\n51\r\n"
+                );
+            }
+            Err(_) => {
+                eprintln!("timeout reached");
+                assert!(false)
+            }
+        }
+    }
+}
